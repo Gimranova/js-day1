4429661c5404a9b513b73efd5393cf98b4269cc7
Gimranova <dinarische@icloud.com>
2024-10-12 20:16:04 +0300

    выполняет домашку 1 дня занятий

---
+++ script.js
@@ -0,0 +1,67 @@
+/*let age = 11;
+console.log(age);
+console.log(1 + 9);
+
+let userName = "д'Артаньян";
+console.log(userName);*/
+
+/*let num = 0;
+
+while (num < 10) {
+  console.log(num);
+  num++;
+}*/
+
+/*let counter = 1;
+while (counter < 5) {
+  console.log(`Counter is ${counter}`);
+  counter++;
+}*/
+
+/*for (let i = 1; i < 10; i++) {
+  console.log(`${i}`);
+}
+
+let fruits = ["bananas", "orange", "cocos"];*/
+
+/*for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}*/
+
+/*let numbers = [10, 1, 8, 2, 3, 7, 4, 5, 6];
+
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+console.log(sum);*/ /*
+
+let fruits = ["bananas", "orange", "cocos", "kotlety"];
+let isKotletyAvailabel = false;
+for (let i = 0; i < fruits.length; i++) {
+  if (fruits[i] === "kotlety") {
+    isKotletyAvailabe = true;
+    break;
+  }
+}
+
+isKotletyAvailabe ? console.log("yes") : console.log("none");*/
+
+/*let fruits = ["bananas", "orange", "cocos", "kotlety"];
+
+/*for (let i = 0; i < fruits.length; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  console.log(fruits[i]);
+}*/
+
+/*for (let fruit of fruits) {
+  console.log(fruit);
+}*/
+
+/*let fruitsStr = "";
+for (let fruit of fruits) {
+  fruitsStr += fruit;
+}
+console.log(fruits.join(", "));*/
